0b2dbf4e32ec10ef5968feb85cfa3f9917194db0
gabbley <baniquedg@bishopireton.org>
2017-02-15 11:55:11 -0500

    Added the code for the text to speech

--- app/src/main/java/com/bishopireton/texttospeech/MainActivity.java
+++ app/src/main/java/com/bishopireton/texttospeech/MainActivity.java
@@ -1,13 +1,46 @@
 package com.bishopireton.texttospeech;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    TextToSpeech textToSpeech;
+    EditText txtRead;
+    Button btnRead;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtRead = (EditText)findViewById(R.id.txtReadMe);
+        btnRead = (Button)findViewById(R.id.btnReadMe);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = txtRead.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
     }
 }
+
