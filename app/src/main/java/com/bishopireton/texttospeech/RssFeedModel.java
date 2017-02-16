79f3d4493bf6fbcaf0ff936f023bebcc0f755f35
gabbley <baniquedg@bishopireton.org>
2017-02-15 20:24:54 -0500

    Second Attempt at adding the code to fetch the xml, uses Android Authority Code

---
+++ app/src/main/java/com/bishopireton/texttospeech/RssFeedModel.java
@@ -0,0 +1,18 @@
+package com.bishopireton.texttospeech;
+
+/**
+ * Created by kellyt on 2/15/2017.
+ */
+
+public class RssFeedModel {
+
+    public String title;
+    public String link;
+    public String description;
+
+    public RssFeedModel(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+}
